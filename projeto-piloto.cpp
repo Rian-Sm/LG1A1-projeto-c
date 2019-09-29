81a48c4e3d73fa0347b7d30093414cf33d0f0840
rian santos <riansm100@gmail.com>
2019-09-29 10:14:23 -0300

    criado o ignore || adicionadas biblioteca e loop de menu

--- projeto-piloto.cpp
+++ projeto-piloto.cpp
@@ -3,18 +3,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <string.h>
 
 using namespace std;
+//fotmulario
+typedef struct {char name[20]; char prontuario[20];}ficha;
 
-typedef struct {
-	char name[20];
-	char prontuario[20];
-}ficha;
+//funções externas
+void cadastrar();
+void view();
+void realizar();
+void sobre();
+
 int main () {
 	
-	int  i = 0;
 	char choise;
 	ficha pessoa;
+	int i;
+	for(i=0 ; i < 1 ; ) 
+	{
 		system("cls");
 		
 		cout << "\n\ndigite seu nome ->" << " ";
@@ -24,49 +31,53 @@ int main () {
 		cin >> pessoa.prontuario;
 		
 		cout << "\nseu nome e prontuario -> " << pessoa.name << pessoa.prontuario;
-	
-	do {
-		if(i == 2 )
-			system("cls");
 		
+		
+	}
+	int  i = 2;
+	do{
+		if(i == 2 ){
+			system("cls");
+		}
 		cout << "\n=================================================";
 		cout << "\n       TESTE DOS SISTEMAS REPRESENTACIONIS";
 		cout << "\n=================================================";
-		cout << "\n           1. CADASTRAR QUETIONARIO";
-		cout << "\n           2. VISUALIZAR QUESIONARIO";
-		cout << "\n           3. REALIZAR TESTE";
-		cout << "\n           4. SOBRE O TESTE";
-		cout << "\n           F. FIM";
+		cout << "\n            1. CADASTRAR QUETIONARIO";
+		cout << "\n            2. VISUALIZAR QUESIONARIO";
+		cout << "\n            3. REALIZAR TESTE";
+		cout << "\n            4. SOBRE O TESTE";
+		cout << "\n            F. FIM";
 		cout << "\n=================================================";
 		cout << "\n escolha ->";
 		cin >> choise;
+		i++;
 	}
-		while(i == 1 || i ==2);
-		
-		switch(choise) 
-		{
-			
-			case '1': {
-				break;
-			}
-			case '2' :{
-				break;
-			}
-			case '3' :{
-				break;
-			}
-			case '4' :{
-				break;
-			}
-			case 'f' :{
-				break;
-			}
-			default :{
-				cout << "\n\n erro na escolha ";
-				i=i+2;
-				break;
-			}
+	while(i <=2 );
+	
+	switch(choise) {
+	
+		case '1': {
+			cout << "\nescolheu a 1\n";	
+			break;
 		}
-
+		case '2' :{
+			break;
+		}
+		case '3' :{
+			break;
+		}
+		case '4' :{
+			break;
+		}
+		case 'f' :{
+			break;
+		}
+		default :{
+			cout << "\n\n erro na escolha \n\n";
+			i--;
+			cout << "numero = " << i;
+		}
+	}
+		
 }
 
