694586a5136ebcbcd4b77fabe7d3daf8b88eee1e
rian santos <riansm100@gmail.com>
2019-10-10 12:43:05 -0300

    criada a corpo principal do cadastro

--- cadastrar_body.cpp
+++ cadastrar_body.cpp
@@ -7,10 +7,11 @@
 using namespace std;
 //dados universais
 typedef struct {
-	char nome[20]; char prontuario[20];
+	char nome[20];
+	char prontuario[20];
 }ficha;
 //variaveis universais
-int i, bi;
+int i, bi, n;
 // função de cadastro
 int main() {
 	
@@ -19,12 +20,21 @@ int main() {
 	char binary = 0;
 	ficha pessoa;
 	FILE *arq;
+	char nomeA[50];
 
-	for(i = 0 ; i < 1 ; ){
+	for(i=0 ; i<1 ; ){
 		
 		cout << "\n\n digite seu nome -> ";
 		fflush(stdin);
 		gets (pessoa.nome);
+		
+		n=0;
+		while (n < strlen(pessoa.nome)){
+			if (pessoa.nome[n] == ' '){
+				pessoa.nome[n] = '_';
+			}
+		n++;
+		}
 		
 		if(strcmp(password, pessoa.nome) == 0){
 			cout << "\n parabens, saiu do codigo";
@@ -32,8 +42,18 @@ int main() {
 		}
 		else{
 			cout << "\n digite seu prontuario ->";
-			cin >> pessoa.prontuario;	
+			cin >> pessoa.prontuario;
+			
+			arq = fopen ("Resultado_%s_%s.txt","w");pessoa.nome, pessoa.prontuario;
+			
+			if (arq == NULL){
+				cout << "erro na criacao do arquivo!";
+			}
+			else {
+				cout << "arquivo criado com sucesso!";
+			}
 		}
+		
 	}
 }
 
