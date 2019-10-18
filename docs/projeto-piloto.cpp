7be4f1c77b42ff3270e5e094113564cdf4346250
rian santos <riansm100@gmail.com>
2019-10-18 05:44:31 -0300

    correção no design do menu || cadastro de usuario movido para dentro de uma pasta

--- docs/projeto-piloto.cpp
+++ docs/projeto-piloto.cpp
@@ -20,70 +20,15 @@ int i, bi ;
 
 //função main
 int main ()
-{
-	char password[] = "master";
+{	
+	i = 0;
 	char choise;
-	char binary = 0;
-	ficha pessoa;
-	
-	for(i=0 ; i <= 1 ; ) {
-		
-		if (i == 0){
-			system("cls");
-		}
-		else {
-		}
-		
-		for(bi = 0 ; bi < 1 ;){
-			
-			cout << "\n\n digite seu nome ->" << " ";
-			gets(pessoa.name);
-			
-			if (strcmp(pessoa.name, password) == 0) //identificação da chave mestra
-			{
-				i = 2;
-				bi++;
-			}
-			else{ 
-				cout << "\n digite seu prontuario ->" << " ";
-				cin >> pessoa.prontuario;
-				
-				cout << "\nseu nome e prontuario -> " << pessoa.name << pessoa.prontuario;
-				
-				cout << "\n\n queres salvar o cadastro Y/N ->";
-				cin >> binary;
-				
-				
-				switch(binary) {
-			
-				
-					case 'y':
-					case 'Y': 
-						// abrir arquivo e salvar o breguejonsons
-						cout << "\ncadastro feito com sucesso!";
-						i = 1;
-						bi++;
-						break;
-					
-					case 'n':
-					case 'N': 
-						cout << "\nfalha no cadastro!";
-						i = 1;
-						bi++;
-						break;
-					
-					default:
-						cout << "\n erro na escolha de opição";
-						break;
-				}
-			}
-		}
-	}
-
+	// falta referenciar cadastro externo aqui
 	do{
-		if(i == 3 ){
-			system("cls");
+		if(i==0){
+		system("cls");
 		}
+		
 		cout << "\n=================================================";
 		cout << "\n       TESTE DOS SISTEMAS REPRESENTACIONIS";
 		cout << "\n=================================================";
@@ -96,33 +41,33 @@ int main ()
 		cout << "\n escolha ->";
 		cin >> choise;
 		i++;
+		switch(choise) {
+		
+			case '1': 
+				cout << "\nescolheu a 1\n";
+				i++	;
+				break;
+			
+			case '2' :
+				break;
+			
+			case '3' :
+				break;
+			
+			case '4' :
+				break;
+			
+			case 'f' :
+				return 0;
+			
+			default :
+				cout << "\n\n erro na escolha \n\n";
+				i--;
+				cout << "numero = " << i;	
+				break;		
+		}
 	}
-	while(i <=2 );
-	
-	switch(choise) {
-	
-		case '1': 
-			cout << "\nescolheu a 1\n";	
-			break;
-		
-		case '2' :
-			break;
-		
-		case '3' :
-			break;
-		
-		case '4' :
-			break;
-		
-		case 'f' :
-			return 0;
-		
-		default :
-			cout << "\n\n erro na escolha \n\n";
-			i--;
-			cout << "numero = " << i;	
-			break;		
-	}			
+	while(i <=2 );			
 }
 
 //todos direitos reservados ao Rian Santos Macedo
